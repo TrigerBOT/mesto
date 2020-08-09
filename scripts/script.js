cc988dfa3f6656d4447a1ce250771850d6b76d04
Kirill Zhakov <night3596248@gmail.com>
2020-08-09 16:41:01 +0300

    2 try

--- scripts/script.js
+++ scripts/script.js
@@ -3,48 +3,48 @@ let form = document.querySelector(`.form`);
 let formBack = document.querySelector(`.form__background`);
 let formEdit = document.querySelector(`.profile__edit-button`);
 let formClose = document.querySelector(`.form__close`);
-let formSub = document.querySelector(`.form__submit`);
-let name = document.querySelector (`.profile__name`);
+let formSub = document.querySelector(`.form__submit`); 
+let name = document.querySelector(`.profile__name`);
 let about = document.querySelector(`.profile__about`);
-let nameInput = document.querySelector(`.form__name`);
-let aboutInput = document.querySelector(`.form__about`);
+let nameInput = document.querySelector(`.popup__input_name`);
+let aboutInput = document.querySelector(`.popup__input_about`);
 let page = document.querySelector(`.page`);
 let popup = document.querySelector(`.popup`)
 
 
-
-function open(){
-    form.style.display = "flex";
-    formBack.style.display = "flex";
-    popup.style.display = "flex"
-    nameInput.value= name.textContent;
-    aboutInput.value = about.textContent;
-   
+function open() {
     
- }
+    popup.classList.add(`popup_opened`);
+    nameInput.value = name.textContent;
+    aboutInput.value = about.textContent;
+ 
+    
 
- function close(){
-    form.style.display = "none";
-    formBack.style.display = "none";
-    popup.style.display = "none";
- }
-
-
-function save(event){
-    event.preventDefault()
-    name.innerHTML = nameInput.value;
-          //  formEdit = document.querySelector(`.profile__edit-button`);
-//formEdit.addEventListener('click', open );
-    about.textContent = aboutInput.value;
-    close();
 }
-formEdit.addEventListener('click', open );
-formClose.addEventListener('click', close );
-formSub.addEventListener('click', save );
 
-form.addEventListener("keyup", function(event) {
+function close() {
+    popup.classList.remove(`popup_opened`);
+  
+}
+
+
+function save(event) {
     event.preventDefault();
-    if (event.keyCode === 13) {
-        formSub.click();
-    }
-});
+    name.textContent = nameInput.value;
+    about.textContent = aboutInput.value;
+    console.log(name);
+    close();
+   
+}
+function pressEnter(event){
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            formSub.click();
+        }
+    
+}
+
+formEdit.addEventListener('click', open);
+formClose.addEventListener('click', close);
+form.addEventListener('submit', save);
+form.addEventListener("keyup", pressEnter) ;
