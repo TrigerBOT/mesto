ab99bba24b30cf2b978e6da7078f545769759b8f
Kirill Zhakov <night3596248@gmail.com>
2020-08-12 17:54:41 +0300

    3 try

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 
-let form = document.querySelector(`.form`);
-let formBack = document.querySelector(`.form__background`);
+let form = document.querySelector(`.popup__form`);
 let formEdit = document.querySelector(`.profile__edit-button`);
-let formClose = document.querySelector(`.form__close`);
-let formSub = document.querySelector(`.form__submit`); 
+let formClose = document.querySelector(`.popup__close`);
+let formSub = document.querySelector(`.popup__submit`);
 let name = document.querySelector(`.profile__name`);
 let about = document.querySelector(`.profile__about`);
 let nameInput = document.querySelector(`.popup__input_name`);
@@ -13,18 +12,18 @@ let popup = document.querySelector(`.popup`)
 
 
 function open() {
-    
+
     popup.classList.add(`popup_opened`);
     nameInput.value = name.textContent;
     aboutInput.value = about.textContent;
- 
-    
+
+
 
 }
 
 function close() {
     popup.classList.remove(`popup_opened`);
-  
+
 }
 
 
@@ -32,19 +31,18 @@ function save(event) {
     event.preventDefault();
     name.textContent = nameInput.value;
     about.textContent = aboutInput.value;
-    console.log(name);
     close();
-   
+
 }
-function pressEnter(event){
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            formSub.click();
-        }
-    
+function pressEnter(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        formSub.click();
+    }
+
 }
 
 formEdit.addEventListener('click', open);
 formClose.addEventListener('click', close);
 form.addEventListener('submit', save);
-form.addEventListener("keyup", pressEnter) ;
+form.addEventListener("keyup", pressEnter);
